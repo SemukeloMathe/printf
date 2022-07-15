cd31711797111282a503fb6ed1fabfdd6c401a5e
SemTheLegend <semmathe@gmail.com>
2022-07-15 14:40:03 -0700

    added newfiles

--- main.h
+++ main.h
@@ -5,11 +5,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
 * struct op - flag / function object
 * @c: flag
 * @f: function
+*  * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
 */
 
 typedef struct op
@@ -29,6 +36,28 @@ int print_b(va_list binary_list);
 int print_u(va_list ar_list);
 int print_o(va_list ar_list);
 int print_x(va_list ar_list);
+
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 int print_X(va_list ar_list);
 
 #endif
+
